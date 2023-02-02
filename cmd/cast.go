3a2937e402c82459b79570a5d248a31ac386897e
pedro-git-projects <pedro.generic.email@gmail.com>
2023-02-02 16:46:23 -0300

    breaking up cast ray func

--- cmd/cast.go
+++ cmd/cast.go
@@ -7,30 +7,40 @@ import (
 	"github.com/pedro-git-projects/go-raycasting/cmd/utils"
 )
 
-func (app *App) CastRay(angle float64, rayId int) {
-	app.game.Rays()[rayId] = *ray.New(angle) // constructor sanitizes angle
-	ray := app.game.Rays()[rayId]
+// horizontalIntersectionResult represents the data expected when calculating the horizontal intersection
+type horizontalIntersectionResult struct {
+	horzXWallCollision float64
+	horzYWallCollision float64
+	horzWallContent    int32
+	foundHorzCollision bool
+}
 
-	var horzXWallCollision float64
-	var horzYWallCollision float64
-	var horzWallContent int32
-	foundHorzCollision := false
+// verticalIntersectionResult represents the data expected when calculating the vertical intersection
+type verticalIntersectionResult struct {
+	vertXWallCollision float64
+	vertYWallCollision float64
+	vertWallContent    int32
+	foundVertCollision bool
+}
+
+func (app *App) calculateHorizontalIntersection(ray *ray.Ray) *horizontalIntersectionResult {
+	r := &horizontalIntersectionResult{}
 
 	// closest y-coordinate to the horizontal grid intersection
-	yIntersection := math.Floor(app.player.Y()/float64(app.game.TileSize())) * float64(app.game.TileSize())
+	yIntersection := math.Floor(app.player.Y()/float64(app.game.TileSize())) * float64(app.game.TileSize()) //ok
 	if ray.IsFacingDown() {
-		yIntersection += float64(app.game.TileSize())
+		yIntersection += float64(app.game.TileSize()) // ok
 	}
 
 	// closest x-coordinate to the horizontal grid intersection
-	xIntersection := app.player.X() + ((yIntersection - app.player.Y()) / math.Tan(ray.Angle()))
+	xIntersection := app.player.X() + ((yIntersection - app.player.Y()) / math.Tan(ray.Angle())) // ok
 
-	yStep := float64(app.game.TileSize())
+	yStep := float64(app.game.TileSize()) // ok
 	if ray.IsFacingUp() {
 		yStep *= -1
 	}
 
-	xStep := float64(app.game.TileSize()) / math.Tan(ray.Angle())
+	xStep := float64(app.game.TileSize()) / math.Tan(ray.Angle()) // ok
 	if ray.IsFacingLeft() && xStep > 0 {
 		xStep *= -1
 	}
@@ -38,6 +48,7 @@ func (app *App) CastRay(angle float64, rayId int) {
 		xStep *= -1
 	}
 
+	// ok
 	nextHorzXCollision := xIntersection
 	nextHorzYCollision := yIntersection
 
@@ -53,36 +64,34 @@ func (app *App) CastRay(angle float64, rayId int) {
 			yToCheck -= 1
 		}
 		if app.game.IsSolidCoordinate(xToCheck, yToCheck) {
-			horzXWallCollision = nextHorzXCollision
-			horzYWallCollision = nextHorzYCollision
-			horzWallContent = app.game.GameMap()[int(math.Floor(yToCheck/float64(app.game.TileSize())))][int(math.Floor(xToCheck/float64(app.game.TileSize())))]
-			foundHorzCollision = true
+			r.horzXWallCollision = nextHorzXCollision
+			r.horzYWallCollision = nextHorzYCollision
+			r.horzWallContent = app.game.GameMap()[int(math.Floor(yToCheck/float64(app.game.TileSize())))][int(math.Floor(xToCheck/float64(app.game.TileSize())))]
+			r.foundHorzCollision = true
 			break
 		} else {
 			nextHorzXCollision += float64(xStep)
 			nextHorzYCollision += float64(yStep)
 		}
 	}
+	return r
+}
 
-	// Starting vertical calculations
-	var vertXWallCollision float64
-	var vertYWallCollision float64
-	var vertWallContent int32
-	foundVertCollision := false
-
-	xIntersection = math.Floor(app.player.X()/float64(app.game.TileSize())) * float64(app.game.TileSize())
+func (app *App) calculateVerticalIntersection(ray *ray.Ray) *verticalIntersectionResult {
+	r := &verticalIntersectionResult{}
+	xIntersection := math.Floor(app.player.X()/float64(app.game.TileSize())) * float64(app.game.TileSize())
 	if ray.IsFacingRight() {
 		xIntersection += float64(app.game.TileSize())
 	}
 
-	yIntersection = app.player.Y() + ((xIntersection - app.player.X()) * math.Tan(ray.Angle()))
+	yIntersection := app.player.Y() + ((xIntersection - app.player.X()) * math.Tan(ray.Angle()))
 
-	xStep = float64(app.game.TileSize())
+	xStep := float64(app.game.TileSize())
 	if ray.IsFacingLeft() {
 		xStep *= -1
 	}
 
-	yStep = float64(app.game.TileSize()) * math.Tan(ray.Angle())
+	yStep := float64(app.game.TileSize()) * math.Tan(ray.Angle())
 	if ray.IsFacingUp() && yStep > 0 {
 		yStep *= -1
 	}
@@ -104,38 +113,47 @@ func (app *App) CastRay(angle float64, rayId int) {
 		}
 		yToCheck := nextVertYCollision
 		if app.game.IsSolidCoordinate(xToCheck, yToCheck) {
-			vertXWallCollision = nextVertXCollision
-			vertYWallCollision = nextVertYCollision
-			vertWallContent = app.game.GameMap()[int(yToCheck/float64(app.game.TileSize()))][int(xToCheck/float64(app.game.TileSize()))]
-			foundVertCollision = true
+			r.vertXWallCollision = nextVertXCollision
+			r.vertYWallCollision = nextVertYCollision
+			r.vertWallContent = app.game.GameMap()[int(yToCheck/float64(app.game.TileSize()))][int(xToCheck/float64(app.game.TileSize()))]
+			r.foundVertCollision = true
 			break
 		} else {
 			nextVertXCollision += float64(xStep)
 			nextVertYCollision += float64(yStep)
 		}
 	}
+	return r
+}
+
+func (app *App) CastRay(angle float64, rayId int) {
+	app.game.Rays()[rayId] = *ray.New(angle)
+	ray := app.game.Rays()[rayId]
+
+	h := app.calculateHorizontalIntersection(&ray)
+	v := app.calculateVerticalIntersection(&ray)
 
 	horzCollisionDist := math.MaxFloat64
 	vertCollisionDist := math.MaxFloat64
 
-	if foundHorzCollision {
-		horzCollisionDist = utils.DistanceBetweenPoints(app.player.X(), app.player.Y(), horzXWallCollision, horzYWallCollision)
+	if h.foundHorzCollision {
+		horzCollisionDist = utils.DistanceBetweenPoints(app.player.X(), app.player.Y(), h.horzXWallCollision, h.horzYWallCollision)
 	}
-	if foundVertCollision {
-		vertCollisionDist = utils.DistanceBetweenPoints(app.player.X(), app.player.Y(), vertXWallCollision, vertYWallCollision)
+	if v.foundVertCollision {
+		vertCollisionDist = utils.DistanceBetweenPoints(app.player.X(), app.player.Y(), v.vertXWallCollision, v.vertYWallCollision)
 	}
 
 	if vertCollisionDist < horzCollisionDist {
 		app.game.Rays()[rayId].SetDistance(vertCollisionDist)
-		app.game.Rays()[rayId].SetXCollision(vertXWallCollision)
-		app.game.Rays()[rayId].SetYCollision(vertYWallCollision)
-		app.game.Rays()[rayId].SetContent(vertWallContent)
+		app.game.Rays()[rayId].SetXCollision(v.vertXWallCollision)
+		app.game.Rays()[rayId].SetYCollision(v.vertYWallCollision)
+		app.game.Rays()[rayId].SetContent(v.vertWallContent)
 		app.game.Rays()[rayId].SetIsVerticalCollision(true)
 	} else {
 		app.game.Rays()[rayId].SetDistance(horzCollisionDist)
-		app.game.Rays()[rayId].SetXCollision(horzXWallCollision)
-		app.game.Rays()[rayId].SetYCollision(horzYWallCollision)
-		app.game.Rays()[rayId].SetContent(horzWallContent)
+		app.game.Rays()[rayId].SetXCollision(h.horzXWallCollision)
+		app.game.Rays()[rayId].SetYCollision(h.horzYWallCollision)
+		app.game.Rays()[rayId].SetContent(h.horzWallContent)
 		app.game.Rays()[rayId].SetIsVerticalCollision(false)
 	}
 }
