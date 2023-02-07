4a4230bf93e9a9587659cc8a697b4a35ec5d8341
pedro-git-projects <pedro.generic.email@gmail.com>
2023-02-07 01:12:11 -0300

    fixing distance between points calculation

--- cmd/utils/utils.go
+++ cmd/utils/utils.go
@@ -2,7 +2,7 @@ package utils
 
 import "math"
 
-func DistanceBetweenPoints(x0, x1, y0, y1 float64) float64 {
+func DistanceBetweenPoints(x0, y0, x1, y1 float64) float64 {
 	return math.Sqrt((x1-x0)*(x1-x0) + (y1-y0)*(y1-y0))
 }
 
